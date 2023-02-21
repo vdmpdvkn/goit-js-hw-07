c7e276e46adff1e7648d044a8c105d26ba2b1278
Vadim Pudovkin <113547631+vdmpdvkn@users.noreply.github.com>
2023-02-21 21:40:11 +0300

    hw finished

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("beforeend", createImageEl(galleryItems));
+galleryEl.addEventListener("click", selectGalleryEl);
+
+function createImageEl(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<div class = "gallery__item"><a class="gallery__link"><img class = "gallery__image lazyload" src = "${preview}" data-source = "${original}" alt = "${description}" loading = "lazy"></a></div>`
+    )
+    .join("");
+}
+function selectGalleryEl(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  //   console.log(evt.target.dataset.source);
+  const instance = basicLightbox.create(
+    `
+    <img src="${evt.target.dataset.source}" width="800" height="600 loading = "lazy" class = "lazyload" style ="border-radius: 7%">
+`,
+    {
+      closable: true,
+      onShow() {
+        window.addEventListener("keydown", (evt) => {
+          if (evt.code === "Escape") {
+            instance.close();
+          }
+        });
+        document.body.style.overflow = "hidden";
+      },
+      onClose() {
+        window.removeEventListener("keydown", (evt) => {
+          if (evt.code === "Escape") {
+            instance.close();
+          }
+        });
+        document.body.style.overflow = "";
+      },
+    }
+  );
+
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+galleryEl.insertAdjacentHTML("beforeend", createGalleryImage(galleryItems));
+
+function createGalleryImage(images) {
+  return images
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href = "${original}" ><img class = "gallery__image lazyload" src = "${preview}" alt = "${description}">
+    </a>`
+    )
+    .join("");
+}
+
+const libGallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionsDelay: 250,
+});
